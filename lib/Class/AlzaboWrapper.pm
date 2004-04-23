6038472028cdc856bf1a5be2c71e3d77962939a7
Dave Rolsky <autarch@urth.org>
2004-04-23 18:59:15 +0000

    Import of DROLSKY/Class-AlzaboWrapper-0.04 from CPAN.
    gitpan-cpan-distribution: Class-AlzaboWrapper
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         DROLSKY/Class-AlzaboWrapper-0.04.tar.gz
    gitpan-cpan-author:       DROLSKY
    gitpan-cpan-maturity:     released

--- lib/Class/AlzaboWrapper.pm
+++ lib/Class/AlzaboWrapper.pm
@@ -4,7 +4,7 @@ use strict;
 
 use vars qw($VERSION);
 
-$VERSION = 0.03;
+$VERSION = 0.04;
 
 use Class::AlzaboWrapper::Cursor;
 
@@ -78,6 +78,8 @@ sub _make_methods
         no strict 'refs';
         *{"$p{caller}\::table"} = sub { $p{table} };
     }
+
+    $TableToClass{ $p{table}->name } = $p{caller};
 }
 
 sub new
